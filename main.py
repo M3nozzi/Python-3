d673aee82df28871f803630bce0ab8ba79fe2c11
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-14 12:13:35 -0300

    done

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import requests
 
-
 def get_temperature(lat, lng):
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
-    reponse = requests.get(url)
-    data = reponse.json()
+    response = requests.get(url)
+    data = response.json()
     temperature = data.get('currently').get('temperature')
     if not temperature:
         return
--- test_main.py
+++ test_main.py
@@ -1,12 +1,21 @@
 from main import get_temperature
 import pytest
 import requests
-from unittest.mock import Mock
+
 
 class MockResponse:
     @staticmethod
     def json():
-        return{"currently": {"temperature": 62}}
+        return {"currently": {"temperature": 62}}
 
-def test_get_temperature_by_lat_lng():
-    raise NotImplementedError
+@pytest.fixture
+def mock_res(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponse
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+def test_get_temperature_by_lat_lng(mock_res):
+    exp = get_temperature(-14.235004, -51.92528)
+    assert exp == 16
+
